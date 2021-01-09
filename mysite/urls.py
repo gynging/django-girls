f4e7924f90b6c74457defe2abad14500c1167548
森本眞一郎 <shin@MacBook-Pro.local>
2021-01-09 11:34:01 +0900

    03.01.19

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+admin.site.site_header = '森本眞一郎' 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
